e9cfe559a337aae5c389e0e28e1d7d43635d2c26
Herman Adu <herman.adu@hotmail.com>
2022-06-20 15:10:48 +0100

    aded react bootstrap setup navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Nav/Nav';
 
 function App() {
   return (
-    <div className="App">
-      herman
+    <div>
+      <Navbar />
+      
     </div>
   );
 }
